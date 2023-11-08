ff50461869b326a0394ca250df6983c1f4b59769
Nokuthaba <nokuthaba.m@outlook.com>
2023-11-08 23:59:41 +0200

    Changed apps.py docstrings

--- band/apps.py
+++ band/apps.py
@@ -1,13 +1,17 @@
 from django.apps import AppConfig
 
-
 class BandConfig(AppConfig):
     """
-    This class will configure the band app.
-    Parameters:
-        AppConfig: config
-    Attributes:
-        name: str
-            """
+    This class represents the configuration for the band app.
+
+    :param AppConfig: The base configuration class.
+    :type AppConfig: django.apps.AppConfig
+
+    :cvar default_auto_field: The default auto field for models.
+    :vartype default_auto_field: str
+    :cvar name: The name of the app.
+    :vartype name: str
+    """
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'band'
