0a33f9e1a99d2a06a79348228b4b0c3b669f94d1
Nokuthaba <nokuthaba.m@outlook.com>
2023-11-08 23:55:53 +0200

    Changed models.py docstrings

--- band/models.py
+++ band/models.py
@@ -3,13 +3,19 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     """
-    This class will create a post.
-    Parameters:
-        models.Model: model
-    Attributes:
-        title: str
-        content: str
-        date: date
+        This class represents a blog post.
+
+        :param models.Model: The base model class
+        :type models.Model: django.db.models.Model
+
+        :ivar title: The title of the blog post
+        :vartype title: str
+        :ivar content: The content of the blog post
+        :vartype content: str
+        :ivar image: An optional image for the blog post
+        :vartype image: django.db.models.ImageField
+        :ivar date: The date of the blog post creation
+        :vartype date: datetime.date
     """
     title = models.CharField(max_length=100)
     content = models.TextField()
@@ -19,10 +25,12 @@ class Post(models.Model):
     
     def __str__(self):
         """
-        This method will return the title of the post
-        Parameters:
-            self: Post object
-        Returns:
-            title: str
+        Return the title of the post.
+
+        :param self: The Post object
+        :type self: Post
+
+        :return: The title of the post
+        :rtype: str
         """
         return self.title   
