0e2752d97e6160ff2e52881c0b10765656649926
Nokuthaba <nokuthaba.m@outlook.com>
2023-11-08 23:58:48 +0200

    Changed forms.py docstrings

--- band/forms.py
+++ band/forms.py
@@ -2,48 +2,58 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-
 class SignUpForm(forms.Form):
     """
-    This class will create a form for the user to sign up if they do not have an account.
-    Parameters:
-        forms.form: form 
-    Attributes:
-        username: str
-        password: str
-        first_name: str
+    This class defines a user registration form for signing up.
 
-    Methods:
-        clean_username: This method will check if the username is already taken.
-        save: This method will save the user to the database.
+    :param forms.Form: The base form class.
+    :type forms.Form: django.forms.Form
+
+    :ivar username: The desired username for registration.
+    :vartype username: str
+    :ivar password: The password for the new user.
+    :vartype password: str
+    :ivar first_name: The first name of the user.
+    :vartype first_name: str
+
+    :cvar username: The username entered during registration.
+    :vartype username: str
+
+    :methods:
+        - clean_username: Check if the username is already taken.
+        - save: Save the user to the database after registration.
     """
+
     username = forms.CharField(max_length=100)
     password = forms.CharField(max_length=100, widget=forms.PasswordInput)
     first_name = forms.CharField(max_length=100)
 
-
     def clean_username(self):
         """
-        This method will check if the username is already taken
-        Parameters:
-            self: SignUpForm object
-        Returns:
-            username: str
+        Check if the username is already taken.
+
+        :param self: The SignUpForm object.
+        :type self: SignUpForm
+
+        :return: The username if it is available.
+        :rtype: str
+
+        :raises ValidationError: If the username is already taken.
         """
         username = self.cleaned_data.get('username')
         if User.objects.filter(username=username).exists():
-            raise ValidationError("A user with this username already exists. Please choose a different username or login.")
+            raise ValidationError("A user with this username already exists. Please choose a different username or log in.")
         return username
-    
-    
-    #save the user to the database
+
     def save(self):
         """
-        This saves the user to the database after sign up.
-        Parameters:
-            self: SignUpForm object
-        Returns:
-            None
+        Save the user to the database after sign up.
+
+        :param self: The SignUpForm object.
+        :type self: SignUpForm
+
+        :return: The created user object.
+        :rtype: User
         """
         user = User.objects.create_user(
             self.cleaned_data['username'],
